bcf10ec35839274ac41f1c6a9f3e708d676a7c04
matsu123abc <ak_matsuhashi@yahoo.co.jp>
2026-06-05 23:17:58 +0900

    update 2317

--- main.py
+++ main.py
@@ -573,6 +573,7 @@ def index():
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Stock AI Screening Viewer</title>
 
   <style>
